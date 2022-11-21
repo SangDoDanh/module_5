bac3e09255b608602163bf3b9f4e7e88c6953c5b
SangDoDanh <danhsang.it@gmail.com>
2022-11-21 17:50:36 +0700

    up

--- AngularModule5/src/app/calculator/calculator.component.ts
+++ AngularModule5/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {removeSummaryDuplicates} from '@angular/compiler';
 
 @Component({
@@ -7,19 +7,22 @@ import {removeSummaryDuplicates} from '@angular/compiler';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  fontSize = 14;
   numberTwo = '';
   numberOne = '';
   result = '';
-  constructor() { }
+
+  constructor() {
+  }
+
   ngOnInit(): void {
   }
-  check(numberOne: string, numberTow: string): boolean {
+
+  checkNumber(numberOne: string, numberTow: string): boolean {
     return isNaN(parseFloat(numberOne)) === false && isNaN(parseFloat(numberTow)) === false;
   }
 
   sum() {
-    if (this.check(this.numberOne, this.numberTwo)) {
+    if (this.checkNumber(this.numberOne, this.numberTwo)) {
       this.result = parseFloat(this.numberOne) + parseFloat(this.numberTwo) + '';
     } else {
       this.result = 'Number of valid';
@@ -27,7 +30,7 @@ export class CalculatorComponent implements OnInit {
   }
 
   minus() {
-    if (this.check(this.numberOne, this.numberTwo)) {
+    if (this.checkNumber(this.numberOne, this.numberTwo)) {
       this.result = parseFloat(this.numberOne) - parseFloat(this.numberTwo) + '';
     } else {
       this.result = 'Number of valid';
@@ -35,14 +38,15 @@ export class CalculatorComponent implements OnInit {
   }
 
   multi() {
-    if (this.check(this.numberOne, this.numberTwo)) {
+    if (this.checkNumber(this.numberOne, this.numberTwo)) {
       this.result = parseFloat(this.numberOne) * parseFloat(this.numberTwo) + '';
     } else {
       this.result = 'Number of valid';
     }
   }
+
   division() {
-    if (this.check(this.numberOne, this.numberTwo)) {
+    if (this.checkNumber(this.numberOne, this.numberTwo)) {
       if (this.numberTwo === '0') {
         this.result = 'Number tow is not "0"';
       } else {
@@ -51,9 +55,5 @@ export class CalculatorComponent implements OnInit {
     } else {
       this.result = 'Number of valid';
     }
-  }
-
-  changeFontSize(e) {
-    this.fontSize = e.target.value;
   }
 }
